9c02fc51ecacc6524a39442e1c67f1c096c761c5
Baskarm7611 <64820629+Baskarm7611@users.noreply.github.com>
2024-09-04 20:27:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/Thirai_HDTDL/17") 
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002193308019"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
